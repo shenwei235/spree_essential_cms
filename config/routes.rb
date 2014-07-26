749f89be85d27d13bbf7cb777a397d31863920a9
shenwei235 <shenwei235@gmail.com>
2014-07-27 02:18:56 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Spree::Core::Engine.routes.append do
   end
   
   constraints(Spree::PossiblePage) do
-    get '*page_path', :to => 'pages#show', :as => :page
+    get '*page_path', :to => 'pages#show', :as => :cms_page
   end
   
 end
