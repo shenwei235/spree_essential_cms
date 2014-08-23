1a13f7b0c87b7738954ca55741a070ecdce6697f
shenwei235 <shenwei235@gmail.com>
2014-08-23 10:46:36 +0800

    Update page_image.rb

--- app/models/spree/page_image.rb
+++ app/models/spree/page_image.rb
@@ -4,6 +4,9 @@ module Spree
     has_attached_file :attachment,
       :url => '/spree/pages/:id/:style/:basename.:extension',
       :path => ':rails_root/public/spree/pages/:id/:style/:basename.:extension'
+    
+    validates_attachment_content_type :attachment, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 
     def image_content?
       attachment_content_type.to_s.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
